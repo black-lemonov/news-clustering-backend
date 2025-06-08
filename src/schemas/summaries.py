59541c093f0953b263c2d97b670c830d4a8acb23
black-lemonov <egor.porf1@mail.ru>
2025-06-08 10:53:14 +0300

    перенос схем ответов в отдельный модуль

---
+++ src/schemas/summaries.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from src.schemas.base import Pagination
+
+
+class SummaryScheme(BaseModel):
+    title: str
+    summary: str
+    created_at: datetime
+    cluster_n: int
+
+
+class SummarySchemeWithPagination(BaseModel):
+    data: list[SummaryScheme]
+    pagination: Pagination
+
+
+class SourceScheme(BaseModel):
+    url: str
+    title: str
+
+
+class SummaryWithSourcesScheme(SummaryScheme):
+    news: list[SourceScheme]
