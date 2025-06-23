1f8c1d7e619f10dc6f63f716912bcf0acf70bc8f
black-lemonov <egor.porf1@mail.ru>
2025-06-24 01:06:36 +0300

    убрано создание схем в return

--- src/routers/ratings.py
+++ src/routers/ratings.py
@@ -2,18 +2,23 @@ from fastapi import APIRouter
 
 from src.dependencies import SessionDep
 from src.dto.rating import RateType, RateAction
+from src.dto.responses import BaseResponse
 from src.services.rating_service import update_summary_rate
 
 
 ratings_router = APIRouter(prefix="/summaries", tags=["Оценки"])
 
 
-@ratings_router.patch("/{cluster_n}/{rate_type}/{action}", summary="Обновить рейтинг реферата")
+@ratings_router.patch(
+    "/{cluster_n}/{rate_type}/{action}",
+    summary="Обновить рейтинг реферата",
+    response_model=BaseResponse
+)
 async def update_summary_rate_endpoint(
         cluster_n: int,
         rate_type: RateType,
         action: RateAction,
-        session: SessionDep
+        session: SessionDep,
 ):
     await update_summary_rate(
         session,
