70fec2d0ddaf48322e2d77087f0650537125be0a
black-lemonov <egor.porf1@mail.ru>
2025-06-24 01:07:18 +0300

    проект api для админ панели

--- src/routers/admin.py
+++ src/routers/admin.py
@@ -1,55 +1,55 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, File, Body
 
-from src.config import SELECTED_MODEL, SUMM_MODELS_FILEPATHS
+from src.config import SUMM_MODELS_FILEPATHS
 from src.dependencies import SessionDep
-from src.services.news_service import del_cluster_in_news, del_news_by_cluster
+from src.parsers.parsers_selection import get_parsers_sites_urls, remove_parser
+from src.summarizers.utils.model_selection import set_model_by_name, get_selected_model_name
 
 admin_router = APIRouter(prefix="/admin", tags=["Управление 🤖"])
 
 
-@admin_router.delete("/{cluster_n}", summary="Удалить кластер")
-async def del_cluster(
-        cluster_n: int,
-        session: SessionDep
-) -> dict:
-    await del_cluster_in_news(session, cluster_n)
-
-    return {"status": "OK", "message": "Кластер удален"}
-
 @admin_router.post("/summaries/{cluster_n}", summary="Создать новый реферат для кластера")
 async def generate_new_summary(
         cluster_n: int,
-        session: SessionDep
-) -> dict:
-    await del_cluster_in_news(session, cluster_n)
-
+        session: SessionDep,
+        bg_tasks: BackgroundTasks
+):
     return {"status": "OK", "message": "Кластер удален"}
 
 
-@admin_router.delete("/news/{cluster_n}", summary="Удалить кластер и все новости")
-async def del_cluster_w_news(
-        cluster_n: int,
-        session: SessionDep
-) -> dict:
-    await del_news_by_cluster(session, cluster_n)
-
-    return {"status": "OK", "message": "Кластер и новости удалены"}
-
-
 @admin_router.get("/models", summary="Получить список доступных моделей")
-def get_available_models() -> dict:
-    return {"available_models": SUMM_MODELS_FILEPATHS.keys()}
+def get_available_models():
+    return {"available_models": list(SUMM_MODELS_FILEPATHS.keys())}
 
 
 @admin_router.get("/models/selected", summary="Получить название выбранной модели")
-def get_selected_model() -> dict:
-    return {"selected_model": SELECTED_MODEL}
+def get_selected_model():
+    name = get_selected_model_name()
+    return {"selected_model": name}
 
 
 @admin_router.post("/models/selected", summary="Выбрать модель")
-def set_model(model_name: str) -> dict:
-    if SUMM_MODELS_FILEPATHS.get(model_name):
-       SELECTED_MODEL = model_name
-       return {"status": "OK", "message": "Модель успешно установлена"}
+def set_model(model_name: str):
+    set_model_by_name(model_name)
+    return {"status": "OK", "message": "Модель успешно установлена"}
 
-    return {"status": "ERROR", "message": "Такой модели не существует"}
+
+@admin_router.get("/parsers", summary="Получить список новостных сайтов для парсинга")
+def get_all_parsers():
+    return {"sites_urls": get_parsers_sites_urls()}
+
+
+@admin_router.delete("/parsers/{site_url}", summary="Удалить новостной сайт из парсинга")
+def delete_parser(site_url: str):
+    remove_parser(site_url)
+    return {"status": "OK", "message": "Парсер удален"}
+
+
+@admin_router.post("/parsers", summary="Загрузить JSON-файл с конфигурацией парсера")
+def load_parser():
+    pass
+
+
+@admin_router.get("/data", summary="Получить .csv статей и рефератов")
+def load_csv_data():
+    pass
