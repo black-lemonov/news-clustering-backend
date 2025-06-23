89391286bfe9e678a393ec5e666105d6b7ecd95e
black-lemonov <egor.porf1@mail.ru>
2025-06-24 01:02:27 +0300

    переработал в соответствие с новым файлом конфигурации

--- src/config.py
+++ src/config.py
@@ -1,37 +1,5 @@
 from datetime import timedelta
 
-PARSERS = [
-  {
-    "site_url": "https://kuban24.tv/news",
-    "article_selector": "div.news-card",
-    "title_selector": "a.news-card-title::text",
-    "url_selector": "a.news-card-title::attr(href)",
-    "date_selector": "div.news-card-head div.news-card-date::text",
-    "content_selector": "div[itemprop=\"description\"] > p::text",
-    "stop_words": [
-        "ТОП", "Гороскоп", "Успейте воспользоваться",
-        "выгодными условиями", "Подробности по телефону"
-    ],
-    "parse_interval_sec": 10.0,
-    "articles_buffer_size": 30
-  },
-  {
-    "site_url": "https://www.livekuban.ru/news",
-    "article_selector": "div.node--news",
-    "title_selector": "div.node--description span::text",
-    "url_selector": "div.node--description a::attr(href)",
-    "date_selector": "div.date::text",
-    "content_selector": "div.article-content > p::text",
-    "stop_words": [
-        "ТОП", "Гороскоп", "пока вы", "Сроки действия акции", "акции",
-        "Успейте воспользоваться",  "выгодными условиями",
-        "Подробности по телефону", "По вопросам публикаций обращайтесь"
-    ],
-    "parse_interval_sec": 10.0,
-    "articles_buffer_size": 30
-  }
-]
-
 MAX_DF = 0.7
 MIN_DF = 1
 EPS = 1.17
@@ -76,10 +44,8 @@ LOGGING_CONFIG = {
 
 SUMM_MODELS_FILEPATHS = {
     "dt": "ml_models/best_dt.joblib",
+    "dt_smote": "ml_models/best_smote_dt.joblib",
     "rf": "ml_models/best_rf.joblib",
     "xgb": "ml_models/best_xgb.json",
     "lgbm": "ml_models/best_lgbm.joblib",
-    "text-rank": ""
 }
-
-SELECTED_MODEL = "lgbm"
--- src/dependencies.py
+++ src/dependencies.py
@@ -7,16 +7,15 @@ from sklearn.cluster import DBSCAN
 from sklearn.pipeline import Pipeline, make_pipeline
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.config import PARSERS, MAX_DF, MIN_DF, EPS, MIN_SAMPLES, SELECTED_MODEL
+from src.config import MAX_DF, MIN_DF, EPS, MIN_SAMPLES, SUMM_MODELS_FILEPATHS
 from src.database import get_session
 from src.parsers.news_parser import NewsParser
+from src.parsers.parsers_factories import JSONParsersFactory
 from src.summarizers.base_summarizer import BaseSummarizer
 from src.preprocessing.tfidf_vectorizer import StemmedTfidfVectorizer
-from src.summarizers.dt_summarizer import DecisionTreeSummarizer
-from src.summarizers.lgbm_summarizer import LGBMSummarizer
-from src.summarizers.rf_summarizer import RandomForestSummarizer
-from src.summarizers.tr_summarizer import TRSummarizer
-from src.summarizers.xgb_summarizer import XGBoostSummarizer
+from src.summarizers.model_summarizer import ModelSummarizer
+from src.summarizers.utils.joblib_loader import JoblibLoader
+from src.summarizers.utils.model_selection import get_selected_model_name
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
@@ -26,11 +25,9 @@ def get_logger() -> Logger:
 
 
 def get_parsers() -> list[NewsParser]:
-    parsers = [
-        NewsParser(**config)
-        for config in PARSERS
-    ]
+    parsers = JSONParsersFactory().load_parsers()
     return parsers
+
 
 def get_clustering_model() -> Pipeline:
     vectorizer = (
@@ -50,11 +47,9 @@ def get_clustering_model() -> Pipeline:
 
 
 def get_summarizer() -> BaseSummarizer:
-    models = {
-        "dt": DecisionTreeSummarizer(),
-        "rf": RandomForestSummarizer(),
-        "xgb": XGBoostSummarizer(),
-        "lgbm": LGBMSummarizer(),
-        "text-rank": TRSummarizer()
-    }
-    return models[SELECTED_MODEL]
+    model = ModelSummarizer(
+        SUMM_MODELS_FILEPATHS[get_selected_model_name()],
+        JoblibLoader()
+    )
+    return model
+
