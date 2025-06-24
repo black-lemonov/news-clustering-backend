04918133b6fb1b3ea79584bca785e55913edc2f4
black-lemonov <egor.porf1@mail.ru>
2025-06-24 13:28:56 +0300

    исправлено название функции

--- src/lifespan.py
+++ src/lifespan.py
@@ -6,13 +6,13 @@ from fastapi import FastAPI
 
 from src.database import init_db
 from src.logger import init_logger
-from src.services.background_service import start_background_task
+from src.services.bg_service import start_bg_task
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     nltk.download("stopwords")
     init_logger()
-    # await init_db()
-    # asyncio.create_task(start_background_task())
+    await init_db()
+    asyncio.create_task(start_bg_task())
     yield
