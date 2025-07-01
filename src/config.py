3d0f185cb404a064e785737e7ddc0aeb68a33239
black-lemonov <egor.porf1@mail.ru>
2025-07-01 14:02:27 +0300

    переименовал переменные окружения

--- src/config.py
+++ src/config.py
@@ -40,11 +40,11 @@ LOGGING_CONFIG = {
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 DB_URL = URL.create(
     "postgresql+asyncpg",
